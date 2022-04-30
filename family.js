22f896feb8ca37c46223c16436479a40ddcc0520
Ruman Sukhera <92282309+Rumam786@users.noreply.github.com>
2022-05-01 00:59:24 +0500

    Add files via upload

---
+++ family.js
@@ -0,0 +1,9 @@
+console.time("test");
+var x = 20;
+console.log(x);
+console.log(100);
+console.table([1,2,3]);
+console.error("Something Error");
+console.warn("This is just warning");
+//console.clear();
+console.timeEnd("test");
